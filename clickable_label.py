ddec75443c820e6a241929dcb0fa87c55531baf3
Aung Myint Myat <amm926616@gmail.com>
2024-08-29 20:23:59 +0630

    added custom class for clickable label

---
+++ clickable_label.py
@@ -0,0 +1,14 @@
+from PyQt6.QtWidgets import QLabel
+from PyQt6.QtCore import pyqtSignal, Qt
+from PyQt6.QtGui import QPixmap
+
+class ClickableLabel(QLabel):
+    doubleClicked = pyqtSignal()  # Signal to emit on double click
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+    def mouseDoubleClickEvent(self, event):
+        if event.button() == Qt.MouseButton.LeftButton:
+            self.doubleClicked.emit()  # Emit signal on double click
+            super().mouseDoubleClickEvent(event)
