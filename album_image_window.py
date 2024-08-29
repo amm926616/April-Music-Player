e88dee2024321ed6d9cfbc2c5e3cbb8bed83d9c9
Aung Myint Myat <amm926616@gmail.com>
2024-08-29 21:16:39 +0630

    added album image window class

---
+++ album_image_window.py
@@ -0,0 +1,63 @@
+from PyQt6.QtWidgets import QDialog, QLabel, QVBoxLayout, QPushButton, QMessageBox
+from PyQt6.QtGui import QIcon, QPixmap
+from PyQt6.QtCore import Qt, QStandardPaths
+import os
+
+class AlbumImageWindow(QDialog):
+    def __init__(self, parent=None, image=None, icon=None, imagename=None):
+        super().__init__(parent)
+        self.image = image
+        self.image_name = imagename
+
+        # Set up the dialog window
+        self.setWindowTitle("Album Image - Full Resolution")
+        if icon:
+            self.setWindowIcon(QIcon(icon))
+
+        # Create a label to display the image
+        image_label = QLabel(self)
+        save_button = QPushButton("Save Image to Disk")
+        
+        if image:
+            image_label.setPixmap(image)
+
+        # Optional: Align the image to the center
+        image_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        # Create and set the layout for the dialog
+        layout = QVBoxLayout()
+        layout.addWidget(image_label)
+        layout.addWidget(save_button)
+        self.setLayout(layout)
+
+        # Optional: Set the size of the window to fit the image
+        self.adjustSize()
+        
+        save_button.clicked.connect(self.save_image)
+
+    def save_image(self):
+        default_image_folder = QStandardPaths.writableLocation(QStandardPaths.StandardLocation.PicturesLocation)
+        
+        # Define the file name and the full path
+        file_name = self.image_name.split('/')[-1]
+        for ext in ['.mp3', '.ogg', '.asc']:
+            if file_name.endswith(ext):
+                file_name = file_name.removesuffix(ext)
+                
+        save_path = os.path.join(default_image_folder, file_name) + ".png"
+        
+        # Save the pixmap to the specified path in PNG format
+        success = self.image.save(save_path, format='PNG')
+
+        # Create a QMessageBox to notify the user
+        msg_box = QMessageBox(self)
+        if success:
+            msg_box.setIcon(QMessageBox.Icon.Information)
+            msg_box.setText(f"Image saved successfully at {save_path}")
+            msg_box.setWindowTitle("Success")
+        else:
+            msg_box.setIcon(QMessageBox.Icon.Critical)
+            msg_box.setText("Failed to save the image.")
+            msg_box.setWindowTitle("Error")
+        
+        msg_box.exec()
