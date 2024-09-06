0af730d19eb5b7ca60320906ae39de7b16913855
Aung Myint Myat <amm926616@gmail.com>
2024-09-06 15:43:09 +0630

    progressbar for loading songs added

---
+++ loadingbar.py
@@ -0,0 +1,15 @@
+from PyQt6.QtWidgets import QProgressDialog, QApplication
+
+class LoadingBar(QProgressDialog):
+    def __init__(self, parent, n):
+        super().__init__(parent)
+        self.setRange(0, n)
+        self.setLabelText("Processing Music Files...")
+        # self.setCancelButton(None)  # Remove the cancel button if not needed
+        self.setWindowTitle("Initializing Database")
+        self.setModal(True)
+        self.setMinimumDuration(0)  # Ensure the dialog appears immediately
+        
+    def update(self, value):
+        self.setValue(value)
+        QApplication.processEvents()  # Process events to update the UI
