2eadc60af469ca4893f5c4d72f51e3e52d3218f5
Aung Myint Myat <amm926616@gmail.com>
2024-09-09 23:06:47 +0630

    rebuild main single instance class

--- main.py
+++ main.py
@@ -3,68 +3,89 @@ from PyQt6.QtWidgets import QApplication
 from PyQt6.QtCore import QSharedMemory
 from PyQt6.QtNetwork import QLocalServer, QLocalSocket
 import sys
+import os
 
-APP_KEY = 'music_player_unique_key'
+APP_KEY = 'adamlististhebest'
 SERVER_NAME = 'MusicPlayerServer'
 
-shared_memory = QSharedMemory(APP_KEY)
+class SingleInstanceApp:
+    def __init__(self):
+        self.shared_memory = QSharedMemory(APP_KEY)
+        self.server = None
 
-def is_another_instance_running():
-    if shared_memory.attach():
-        return True
-    
-    if shared_memory.create(1):
-        return False
-    else:
-        print("Error creating shared memory:", shared_memory.errorString())
-        return True
+    def is_another_instance_running(self):
+        if self.shared_memory.attach():
+            return True
+        if self.shared_memory.create(1):
+            return False
+        else:
+            print("Error creating shared memory:", self.shared_memory.errorString())
+            return True
+        
+    def load_stylesheet(self):
+        """Load the QSS file from the specified path."""
+        script_path = os.path.dirname(os.path.abspath(__file__))
+        file_path = os.path.join(script_path, "style.qss")
+        with open(file_path, "r") as f:
+            return f.read()        
 
-def bring_up_main_window():
-    # Connect to the local server and send a message to bring up the window
-    socket = QLocalSocket()
-    socket.connectToServer(SERVER_NAME)
-    if socket.waitForConnected(1000):
-        # Send a simple message to trigger the main window
-        socket.write(b'activate_window')
-        socket.flush()
-        socket.waitForBytesWritten(1000)
+    def bring_up_main_window(self):
+        # Connect to the local server and send a message to bring up the window
+        socket = QLocalSocket()
+        socket.connectToServer(SERVER_NAME)
+        if socket.waitForConnected(1000):
+            socket.write(b'activate_window')
+            socket.flush()
+            socket.waitForBytesWritten(1000)
+            socket.disconnectFromServer()
+        else:
+            print("Failed to connect to server:", socket.errorString())
+        socket.close()
+
+    def create_local_server(self, ui):
+        self.server = QLocalServer()
+        if self.server.listen(SERVER_NAME):
+            self.server.newConnection.connect(lambda: self.handle_new_connection(ui))
+        else:
+            print("Error starting server:", self.server.errorString())
+
+    def handle_new_connection(self, ui):
+        socket = self.server.nextPendingConnection()
+        if socket and socket.waitForReadyRead(1000):
+            message = socket.readAll().data().decode()
+            if message == 'activate_window':
+                ui.showMaximized()
+                ui.activateWindow()
+                ui.raise_()
         socket.disconnectFromServer()
+        socket.close()
 
-def create_local_server(ui):
-    # Create a local server to listen for incoming connections from other instances
-    server = QLocalServer()
-    
-    if server.listen(SERVER_NAME):
-        # Handle new connections by bringing up the main window
-        server.newConnection.connect(lambda: handle_new_connection(server, ui))
-    
-    return server
+    def run(self):
+        app = QApplication(sys.argv)
+        
+        # Load QSS stylesheet
+        stylesheet = self.load_stylesheet()
+        app.setStyleSheet(stylesheet)        
 
-def handle_new_connection(server, ui):
-    socket = server.nextPendingConnection()
-    if socket and socket.waitForReadyRead(1000):
-        message = socket.readAll().data().decode()
-        if message == 'activate_window':
-            ui.showMaximized()  # Bring the window to normal state if minimized
-            ui.activateWindow()  # Bring the window to the foreground
-            ui.raise_()  # Ensure it is on top of other windows
-    socket.disconnectFromServer()
+        # If no other instance is running, create the UI and the local server
+        ui = MusicPlayerUI(app)
+        ui.createUI()
+
+        # Set up the local server for future instances to communicate with
+        self.create_local_server(ui)
+
+        # Run the application
+        exit_code = app.exec()
+
+        # Cleanup
+        self.shared_memory.detach()
+        sys.exit(exit_code)
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
+    instance_app = SingleInstanceApp()
     
-    if is_another_instance_running():
-        # If another instance is running, bring up its main window
-        bring_up_main_window()
-        sys.exit(0)  # Exit the new instance
-    
-    # If no other instance is running, create the UI and the local server
-    ui = MusicPlayerUI(app)
-    ui.createUI()
-
-    # Set up the local server for future instances to communicate with
-    server = create_local_server(ui)
-    
-    exit_code = app.exec()
-    shared_memory.detach()
-    sys.exit(exit_code)
+    if instance_app.is_another_instance_running():
+        instance_app.bring_up_main_window()
+        sys.exit(1)  # Exit the new instance
+    else:
+        instance_app.run()
